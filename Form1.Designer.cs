2fd04d4a68aaf6c572607a5d045adcd05f7c330a
Yusuf ÇELİK <57891856+yusufcelik1@users.noreply.github.com>
2022-02-27 02:54:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace CopyTheSelectedFolderAnotherLocation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaynak = new System.Windows.Forms.Button();
+            this.btnHedef = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKaynak = new System.Windows.Forms.TextBox();
+            this.txtHedef = new System.Windows.Forms.TextBox();
+            this.btnKopyala = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKaynak
+            // 
+            this.btnKaynak.Location = new System.Drawing.Point(460, 46);
+            this.btnKaynak.Name = "btnKaynak";
+            this.btnKaynak.Size = new System.Drawing.Size(75, 23);
+            this.btnKaynak.TabIndex = 0;
+            this.btnKaynak.Text = "Dosya Seç";
+            this.btnKaynak.UseVisualStyleBackColor = true;
+            this.btnKaynak.Click += new System.EventHandler(this.btnKaynak_Click);
+            // 
+            // btnHedef
+            // 
+            this.btnHedef.Location = new System.Drawing.Point(460, 86);
+            this.btnHedef.Name = "btnHedef";
+            this.btnHedef.Size = new System.Drawing.Size(75, 23);
+            this.btnHedef.TabIndex = 1;
+            this.btnHedef.Text = "Dosya Seç";
+            this.btnHedef.UseVisualStyleBackColor = true;
+            this.btnHedef.Click += new System.EventHandler(this.btnHedef_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kaynak";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hedef";
+            // 
+            // txtKaynak
+            // 
+            this.txtKaynak.Location = new System.Drawing.Point(177, 49);
+            this.txtKaynak.Name = "txtKaynak";
+            this.txtKaynak.Size = new System.Drawing.Size(256, 20);
+            this.txtKaynak.TabIndex = 4;
+            // 
+            // txtHedef
+            // 
+            this.txtHedef.Location = new System.Drawing.Point(177, 89);
+            this.txtHedef.Name = "txtHedef";
+            this.txtHedef.Size = new System.Drawing.Size(256, 20);
+            this.txtHedef.TabIndex = 5;
+            // 
+            // btnKopyala
+            // 
+            this.btnKopyala.Enabled = false;
+            this.btnKopyala.Location = new System.Drawing.Point(577, 46);
+            this.btnKopyala.Name = "btnKopyala";
+            this.btnKopyala.Size = new System.Drawing.Size(181, 63);
+            this.btnKopyala.TabIndex = 6;
+            this.btnKopyala.Text = "Kopyala";
+            this.btnKopyala.UseVisualStyleBackColor = true;
+            this.btnKopyala.Click += new System.EventHandler(this.btnKopyala_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 173);
+            this.Controls.Add(this.btnKopyala);
+            this.Controls.Add(this.txtHedef);
+            this.Controls.Add(this.txtKaynak);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHedef);
+            this.Controls.Add(this.btnKaynak);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnHedef;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKaynak;
+        private System.Windows.Forms.TextBox txtHedef;
+        private System.Windows.Forms.Button btnKaynak;
+        private System.Windows.Forms.Button btnKopyala;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyTheSelectedFolderAnotherLocation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnKaynak_Click(object sender, EventArgs e)
+        {
+            txtKaynak.Text = "";//If it is wanted to be used again without closing the program, I use it so that the textbox resets itself.
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtKaynak.Text = Path.Combine(fbd.SelectedPath, Path.GetFileName(txtKaynak.Text));
+            }
+        }
+        private void btnHedef_Click(object sender, EventArgs e)
+        {
+            txtHedef.Text = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtHedef.Text = Path.Combine(fbd.SelectedPath, Path.GetFileName(txtHedef.Text));
+                btnKopyala.Enabled = true;
+            }
+        }
+        public static void CopyAll(DirectoryInfo Kaynak, DirectoryInfo Hedef)
+        {
+            Directory.CreateDirectory(Hedef.FullName);
+            foreach (FileInfo fi in Kaynak.GetFiles())
+            {
+                fi.CopyTo(Path.Combine(Hedef.FullName, fi.Name), true);
+            }
+            foreach (DirectoryInfo subDirectory in Kaynak.GetDirectories())
+            {
+                DirectoryInfo SiradakiDosya = Hedef.CreateSubdirectory(subDirectory.Name);
+                CopyAll(subDirectory, SiradakiDosya);
+            }
+        }
+        private void btnKopyala_Click(object sender, EventArgs e)
+        {
+            CopyAll(new DirectoryInfo(txtKaynak.Text), new DirectoryInfo(txtHedef.Text));
+            MessageBox.Show("Seçilen dosya başarıyla kopyalandı.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
+
